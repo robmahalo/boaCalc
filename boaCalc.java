d3d1cdc4fdff2ad2410c1771451de6444a6229bd
Robby Manalo <44278625+robmahalo@users.noreply.github.com>
2021-09-17 13:51:58 -0400

    Creating collectInput method

--- boaCalc.java
+++ boaCalc.java
@@ -5,13 +5,19 @@
   4. Use if statments and reward input to return the appropriate reward
 */
 
+import java.util.Scanner;
+
 class boaCalc {
     private static double travelCredit = 2500;
     private static double giftCard = 3125;
     private static double money = 4166;
     private static double cash = 25;
+
+    private static double points = 0;
+    private static String type = "";
   
     public static double calcPoints(double points, String type) {
+      //TODO Fix logic to return correct reward
       double reward = 0;
       if (type == "travel") {
         reward = (points/travelCredit) * cash;
@@ -22,8 +28,25 @@ class boaCalc {
       }
       return reward;
     }
+
+    public static void collectInput() {
+      Scanner pointInput = new Scanner(System.in);
+      System.out.println("Enter your amount of redeemable points: ");
+      points = pointInput.nextDouble();
+
+      Scanner typeInput = new Scanner(System.in);
+      System.out.println("Enter the type of award you want to receive: ");
+      type = typeInput.nextLine();
+
+      System.out.println("Your redeemable points: " + points);
+      System.out.println("Your type of reward: " + type);
+    }
   
     public static void main(String[] args) {
-      System.out.println("Your reward is: $" + calcPoints(3666, "gift card"));
+      // Collect points and type of award from user
+      collectInput();
+    
+      System.out.println("Hang on, calculating your reward..");
+      System.out.println("Your reward is: $" + calcPoints(points, type));
     }
   }
